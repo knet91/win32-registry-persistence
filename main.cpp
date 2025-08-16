93c9162bab0361176c0a2f0e8ab56d8b86c30be7
knet91 <incidents@tutanota.com>
2025-08-16 20:46:12 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,77 @@
+// SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+// for Windows, when operating system boots up the calculator.exe program launches; additionally with a messagebox
+------------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+#include <windows.h>
+
+void registryCreate(LPCSTR path, LPCSTR name, DWORD type);
+void messagebox(LPCSTR message, LPCSTR title);
+
+int main() {
+    int Persistence { 1 };
+    int MessageBox { 1 };
+
+    switch(Persistence) {
+        case 1:
+            registryCreate("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "calculator", REG_SZ);
+            break;
+        case 2:
+            break;
+    }
+
+    switch(MessageBox) {
+        case 1:
+            messagebox("hey there", "i love calculators");
+            break;
+        case 2:
+            break;
+    }
+
+    return 0;
+}
+
+void messagebox(LPCSTR message, LPCSTR title) {
+    MessageBoxA(NULL, message, title, MB_OK);
+}
+
+void registryCreate(LPCSTR path, LPCSTR name, DWORD type) {
+    HKEY hKey;
+
+    LSTATUS accessPath = RegOpenKeyExA(
+        HKEY_CURRENT_USER,
+        path,
+        0,
+        KEY_ALL_ACCESS,
+        &hKey
+    );
+    switch(accessPath) {
+        case ERROR_SUCCESS:
+            std::cout << "Accessed the path" << std::endl;
+            break;
+        default:
+            std::cout << "Failed to access path" << std::endl;
+            RegCloseKey(hKey);
+            break;
+    }
+
+    LSTATUS setValue = RegSetValueExA(
+        hKey,
+        name,
+        0,
+        type,
+        (LPBYTE)"C:\\Windows\\System32\\calc.exe",
+        sizeof("C:\\Windows\\System32\\calc.exe")
+    );
+    switch(setValue) {
+        case ERROR_SUCCESS:
+            std::cout << "Created key" << std::endl;
+            break;
+        default:
+            std::cout << "Failed to create key" << std::endl;
+            RegCloseKey(hKey);
+            break;
+    }
+
+    RegCloseKey(hKey);
+}
